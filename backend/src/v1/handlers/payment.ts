2a064e645257a306927e411cfe3ddbf8564350a4
DevWurm <devwurm@devwurm.net>
2017-01-04 17:02:29 +0100

    Moving stripe calls into seperate modules

--- backend/src/v1/handlers/payment.ts
+++ backend/src/v1/handlers/payment.ts
@@ -1,9 +1,9 @@
 import { Request, Response, NextFunction } from 'express-serve-static-core';
 import { extend, match } from '../helpers/util';
-import { stripe, mongodb } from '../helpers/providers';
 import { verifyToken } from '../helpers/crypto';
 import { TokenExpired, TokenInvalid } from '../helpers/types';
 import { getUserByEmail } from '../helpers/db';
+import { createCharge, getCustomer, updateCustomer } from '../helpers/stripe';
 
 export function paymentHandler(req: Request & { token: string }, res: Response, next: NextFunction): void {
   // validate user input
@@ -25,60 +25,18 @@ export function paymentHandler(req: Request & { token: string }, res: Response, 
     // if no verification error occurred, the verification result is the payload email
     const email = verification;
 
-    new Promise((resolve, reject) => {
-      // charge customer
-      stripe.charges.create({
-        amount: req.body.amount,
-        currency: "eur",
-        source: req.body.token, // obtained with Stripe.js
-        description: `Credit charge for ${email}`
-      }, (err, charge) => {
-        if (err) {
-          console.error(err);
-          return reject(extend(new Error('Error while charging credit card'), { status: 500 }));
-        }
-
-        resolve(charge.amount);
-      });
-    })
+    createCharge(req.body.amount, req.body.token, email)
       .then((amount: number) => {
         return getUserByEmail(email)
           .catch(reason => Promise.reject(extend(reason, { status: 500 })))
           .then(user => user.stripeId)
-          .then(stripeId => {
-            // get user data from stripe
-            return new Promise((resolve, reject) => {
-              stripe.customers.retrieve(
-                stripeId,
-                (err, customer) => {
-                  if (err) {
-                    console.error(err);
-                    return reject(extend(new Error('Error while receiving user from payment service'), { status: 500 }));
-                  }
-
-                  resolve(customer);
-                }
-              );
-            });
-          })
+          .then(stripeId => getCustomer(stripeId))
           .then((customer: { id: string, metadata: { credits: number } }) => {
             // update user data at stripe
-            return new Promise((resolve, reject) => {
-              stripe.customers.update(
-                customer.id,
-                {
-                  metadata: {
-                    credits: Number(customer.metadata.credits) + Number(amount)
-                  }
-                },
-                (err, _) => {
-                  if (err) {
-                    console.error(err);
-                    return reject(extend(new Error('Error while updating user at payment service'), { status: 500 }));
-                  }
-
-                  resolve();
-                });
+            return updateCustomer(customer.id, {
+              metadata: {
+                credits: Number(customer.metadata.credits) + Number(amount)
+              }
             });
           })
           .then(_ => {
