0d887ee8939c7d51d6b86dc0f44f7281ca5381f8
DevWurm <devwurm@devwurm.net>
2017-01-06 08:33:25 +0100

    Introducing Task 1

--- backend/src/v1/helpers/manifest.ts
+++ backend/src/v1/helpers/manifest.ts
@@ -1,24 +1,5 @@
-import { readFile } from 'fs';
-import { resolve as pathResolve } from 'path';
-
 export type VersionInfo = {version: string, date: string, project: string} | {version: string}
 
 export function getVersionInfo(type: 'long' | 'short'): Promise<VersionInfo> {
-  return new Promise((resolve, reject) => {
-    readFile(pathResolve(__dirname, '../../../manifests/v1.txt'), (err, data) => {
-      if (err) { return reject(err); }
-      resolve(data);
-    });
-  })
-    .then(content => {
-      const lines = content.toString().split(/\r?\n/);
-
-      return lines.reduce((acc, curr) => {
-        const [key, value] = curr.split(/:/);
-        if ((type == 'long' ? ['version', 'date', 'project'] : ['version']).includes(key)) {
-          return Object.assign(acc, {[key]: value.trim()});
-        }
-        return acc;
-      }, {});
-    });
+ // TODO: Read and parse the manifest file
 }
